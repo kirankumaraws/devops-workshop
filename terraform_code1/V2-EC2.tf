7ba6ae3bbe8f3f04a5fd67ead4fe31cfd0d18cfe
kirankumaraws2024 <kirankumaraws2024@gmail.com>
2024-09-13 13:43:15 +0530

    updated version -v2

---
+++ terraform_code1/V2-EC2.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "demo-server"{
+
+    ami = "ami-0182f373e66f89c85"
+    instance_type = "t2.micro"
+    key_name = "dpp"
+    security_groups=["demo-sg"]
+
+}
+
+resource "aws_security_group" "demo-sg" {
+    name="demo-sg"
+    description="Allow SSH traffic"
+    
+
+ingress{
+    description ="ssh access"
+    from_port=22
+    to_port=22
+    protocol="tcp"
+    cidr_blocks=["0.0.0.0/0"]
+}
+
+egress{
+    from_port=0
+    to_port=0
+    protocol="-1"
+    cidr_blocks=["0.0.0.0/0"]
+}
+
+tags ={
+    Name ="ssh-port"
+}
+
+}
