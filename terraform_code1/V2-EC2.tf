0552e49259f106f67f53c724817a88b27caa36b4
kirankumaraws2024 <kirankumaraws2024@gmail.com>
2024-09-13 13:52:08 +0530

    updated

--- terraform_code1/V2-EC2.tf
+++ terraform_code1/V2-EC2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "demo-server"{
 
     ami = "ami-0182f373e66f89c85"
     instance_type = "t2.micro"
-    key_name = "dpp"
+    key_name = "ddp"
     security_groups=["demo-sg"]
 
 }
