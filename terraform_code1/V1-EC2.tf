3dab80b80ea1f32f327b7d087ca7005bba2eabb8
kirankumaraws2024 <kirankumaraws2024@gmail.com>
2024-09-24 16:02:22 +0530

    v1 version

---
+++ terraform_code1/V1-EC2.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+
+    region = "us-east-1"
+
+}
+
+resource "aws_instance" "demo-server" {
+
+    ami = "ami-0182f373e66f89c85"
+    instance_type = "t2.micro"
+    key_name = "ddp"
+    
+}
